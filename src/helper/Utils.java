10fd7b8f3d58f29d79cdb3043d2cba1ed0776cd7
Lucas Machado de Souza <lcsmachaado@gmail.com>
2023-07-11 16:13:55 -0300

    Utils for cleaning code

---
+++ src/helper/Utils.java
@@ -0,0 +1,14 @@
+package helper;
+
+import java.math.BigDecimal;
+
+public class Utils {
+
+    public final static BigDecimal CEM_REAIS = BigDecimal.valueOf(100.00);
+    public final static BigDecimal CINQUENTA_REAIS = BigDecimal.valueOf(50.00);
+    public final static BigDecimal VINTE_REAIS = BigDecimal.valueOf(20.00);
+    public final static BigDecimal DEZ_REAIS = BigDecimal.valueOf(10.00);
+    public final static BigDecimal CINCO_REAIS = BigDecimal.valueOf(5.00);
+
+
+}
